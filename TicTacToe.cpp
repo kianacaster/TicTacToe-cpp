f7af0d426fc4730039354c23865996f7ebf74947
Kian Acaster <acasterkian@gmail.com>
2019-03-24 23:01:27 +0000

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,17 +4,17 @@
 #include <iostream>
 using namespace std;
 
-char player = 'X'; 
+char player = 'X';
 char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' }; // The grid which the game is played on
 
 // Display the grid
-void Draw() 
+void Draw()
 {
 	system("CLS");
 	cout << "Tic Tac Toe" << endl;
-	for (int i = 0; i < 3; i++) 
+	for (int i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < 3; j++) 
+		for (int j = 0; j < 3; j++)
 		{
 			cout << matrix[i][j] << " ";
 		}
@@ -26,7 +26,7 @@ void Draw()
 // The 'if' statements check to see if the selected space is filled or not
 // If the space is filled, the move is invalid and doesn't count.
 // Return of 0 = Valid Move, 1 = Invalid Move
-int Input() 
+int Input()
 {
 	int a;
 	cout << "Press the number of the square you want...";
@@ -63,7 +63,7 @@ int Input()
 }
 
 // Switches turns; if the last turn was X, change to O - vice versa.
-void TogglePlayer() 
+void TogglePlayer()
 {
 	if (player == 'X')
 		player = 'O';
@@ -72,17 +72,17 @@ void TogglePlayer()
 }
 
 // Checks if there is a line of 3 from either the Xs or Os - if so, returns who won.
-int CheckWin() 
+int CheckWin()
 {
 	// Horizontal
 	if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X') return 1;
-	else if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X') return 1; 
-	else if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X') return 1;	
-	
+	else if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X') return 1;
+	else if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X') return 1;
+
 	else if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X') return 1;
-	else if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X') return 1; 
+	else if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X') return 1;
 	else if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X') return 1;
-	
+
 	// Vertical 
 	else if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O') return 2;
 	else if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O') return 2;
@@ -91,13 +91,13 @@ int CheckWin()
 	else if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O') return 2;
 	else if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O') return 2;
 	else if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O') return 2;
-	
+
 	// Diagonal
 	else if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X') return 1;
-	else if (matrix[0][2] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X') return 1;
-	
+	else if (matrix[0][2] == 'X' && matrix[1][1] == 'X' && matrix[2][0] == 'X') return 1;
+
 	else if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O') return 2;
-	else if (matrix[0][2] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O') return 2;
+	else if (matrix[0][2] == 'O' && matrix[1][1] == 'O' && matrix[2][0] == 'O') return 2;
 
 	else return 0;
 
@@ -107,7 +107,7 @@ int CheckWin()
 int main()
 {
 	Draw();
-	while (1) 
+	while (1)
 	{
 		int input = Input();
 		Draw();
